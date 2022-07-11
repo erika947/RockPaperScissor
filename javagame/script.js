94f8cd022fbbbb6df207e97297538e78daa90320
Erika Rivera <rivera.erika07@gmail.com>
2022-07-11 17:05:36 -0400

    new update

--- javagame/script.js
+++ javagame/script.js
@@ -1,67 +1,88 @@
 // console.log('Hello World');
-const options = ['rock','paper','scissors']
-
-function game(){
-    playRound()
-}
-
-function playerChoice(){//lets the player make a choice
-    let input = prompt("Pick your choice of rock, paper, or sicssors")
-    while (input == null){
-      input = prompt("Pick your choice of rock, paper, or sicssors")
-    }
-    input = input.toLowerCase()
-}
-
-function computerChoice(){//randomchoice from the computer
-     return options[Math.floor(Math.random()*options.length)]
-     console.log(computerChoice)
-}
-
-function playRound(){
-    const playerSelection = playerChoice()
-    const computerSelection = computerChoice()
-    const winner = whoWon(playerSelection,computerSelection)
-    console.log(winner)
-    // for (let i = 0; i < 5; i++){
-
-    // }
-}
-
-function whoWon(playersPick,computersPick){
-    if(playersPick === computersPick){//player choice is the same as the computer then its a tie
-    return "It's a Tie!"
-}
-else if ( playersPick === 'rock' && computersPick == 'sicssors'){//player picks rock and the computer picks sicssors then the player wins that round
-    return "Player"
-}
-else if ( playersPick === 'paper' && computersPick == 'rock'){//player picks paper and the computer picks rock then the player wins that round
-    return "Player"
-}
-else if ( playersPick === 'sicssors' && computersPick == 'paper'){//player picks sicssors and the computer picks paper then the player wins that round
-    return "Player"
-} 
-else
-return "Computer"//anything other option would lead to the computer winning 
-}
 
 
+//create a game function that will help with starting the game as well with making a button so that the game can also be triggered through that
 
+//create a fuction that will have the amount of match(5)
 
+// a prompt that will allow the player to type what they want to pick
 
-
-
-game()
-
-
-
+//also a function that will be able to randomize the choices that the computer will pick
 
 //player chooses rock and computer chooses rock its a tie
 //player chooses paper and computer chooses paper its a tie
 //player chooses sicssor and computer chooses sicssor its a tie
-//player chooses rock and computer chooses paper the computer wins
-//player chooses paper and computer chooses sicssor the computer wins
-//player chooses sicssor and computer chooses rock the computer wins
+
+//don't have to type out all of the 'computer wins' once the players wins section is compeleted because if either of the players choice are met with the condition then it will stop but if it doesn't then that either means that there is a tie or that the computer has one
+
 //player chooses rock and computer chooses sicssor the player wins
 //player chooses paper and computer chooses rock the player wins
 //player choose sicssor and the computer chooses paper the player wins
+//player chooses rock and computer chooses paper the computer wins
+//player chooses paper and computer chooses sicssor the computer wins
+//player chooses sicssor and computer chooses rock the computer wins
+
+//figure out a way to that who won and how many rounds there are
+
+const options = ['rock','paper','scissors']
+let winners = []
+
+function game(){
+    for (let i = 0; i < 5; i++){//set 5 rounds
+    playRound(i)
+    }
+    document.querySelector('button').textContent = 'play new game'
+    
+}
+
+ 
+function playerChoice() {
+  let input = prompt("What will you pick? Rock, Paper, or Scissors");
+  while (input == null) {
+    input = prompt("What will you pick? Rock, Paper, or Scissors");
+  }
+  input = input.toLowerCase();
+  return input;
+}
+
+
+function validateInput(options) {
+  return options.includes(options);
+}
+
+function computerChoice() {
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function playRound(round){
+    const playerSelection = playerChoice()
+    const computerSelection = computerChoice()
+    const winner = whoWon(playerSelection,computerSelection)
+    winners.push(winner)
+    logRounds(round, winner)
+}
+
+function whoWon(playersPick,computersPick){
+  if (playersPick === computersPick) {
+    return 'Tie'
+  }
+  else if (playersPick === 'rock' && computersPick== 'sicssors'){
+    return 'Player wins'
+  }
+  else if (playersPick === 'paper' && computersPick == 'rock'){
+    return 'Player wins'
+  }
+  else if (playersPick === 'sicssors' && computersPick == 'paper'){
+    return 'Player'
+  }
+  else{
+    return 'Computer'
+  }
+}
+
+    function logRounds(round, winner){
+        console.log("Round:", round) 
+       console.log(winner, "won this match!")
+    }
+
+//game()
